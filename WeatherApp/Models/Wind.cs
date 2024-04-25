7e58d5b4551ed3cb86779faf55685142e119089a
Nicole Schlabbers <n.schlabbers@freenet.de>
2024-04-25 10:56:44 +0200

    Added class Wind

---
+++ WeatherApp/Models/Wind.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    public class Wind : NotifyProperty
+    {
+        private string _windDirection = string.Empty;
+        private string _windSpeed = string.Empty;
+
+
+    }
+}
