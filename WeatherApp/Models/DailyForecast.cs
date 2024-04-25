1d29ec35fdf943a9b6b49ccc8931a62ab938bad4
Nicole Schlabbers <n.schlabbers@freenet.de>
2024-04-25 10:56:06 +0200

    Added class DailyForecast

---
+++ WeatherApp/Models/DailyForecast.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Helpers;
+using WeatherApp.Collections;
+
+namespace WeatherApp.Models
+{
+    public class DailyForecast : NotifyProperty
+    {
+        private string _maxTemperatur = string.Empty;
+        private string _minTemperatur = string.Empty;
+
+        private Weekday _day;
+        private string _dayForecast = string.Empty;
+
+
+        public string MaxTemperatur 
+        { 
+            get
+            {
+                return _maxTemperatur;
+            } 
+            set
+            {
+                _maxTemperatur = value;
+                OnPropertyChanged(nameof(MaxTemperatur));
+            }
+        }
+
+        public string MinTemperatur
+        {
+            get
+            {
+                return _minTemperatur;
+            }
+            set
+            {
+                _minTemperatur = value;
+                OnPropertyChanged(nameof(MinTemperatur));
+            }
+        }
+
+        public Weekday Day
+        {
+            get
+            {
+                return _day;
+            }
+            set
+            {
+                _day = value;
+                OnPropertyChanged(nameof(Day));
+            }
+        }
+
+        public string DayForecast
+        {
+            get
+            {
+                return _dayForecast;
+            }
+            set
+            {
+                _dayForecast = value;
+                OnPropertyChanged(nameof(DayForecast));
+            }
+        }
+    }
+}
