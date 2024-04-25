b06c0d016e872968484050a6f78efcb836cc9b22
Nicole Schlabbers <n.schlabbers@freenet.de>
2024-04-25 13:10:32 +0200

    Added enum Winddirection

---
+++ WeatherApp/Collections/Winddirection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Collections
+{
+    public enum Winddirection
+    {
+        N,
+        NO,
+        O,
+        SO,
+        S,
+        SW,
+        W,
+        NW
+    }
+}
