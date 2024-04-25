12347c4d996041433b5752c870d4ffda2cf68f31
Nicole Schlabbers <n.schlabbers@freenet.de>
2024-04-25 09:17:01 +0200

    Added Models ordner with class Temperatur

---
+++ WeatherApp/Models/Temperatur.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    public class Temperatur
+    {
+        private string _maxTemperatur = string.Empty;
+        private string _minTemperatur = string.Empty;
+
+        public string MaxTemperatur 
+        { 
+            get
+            {
+                return _maxTemperatur;
+            } 
+            set
+            {
+
+            }
+        }
+
+        public string MinTemperatur
+        {
+            get
+            {
+                return _minTemperatur;
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
