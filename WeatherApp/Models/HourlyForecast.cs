f04a327e309955d4a37402a4f7701be566929370
Nicole Schlabbers <n.schlabbers@freenet.de>
2024-04-25 13:11:36 +0200

    Added class Hourlyforecast.cs

---
+++ WeatherApp/Models/HourlyForecast.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Collections;
+using WeatherApp.Helpers;
+
+namespace WeatherApp.Models
+{
+    public class HourlyForecast : NotifyPropertyHelper
+    {
+        private string _temperatur = "25";
+
+        // für die Uhrzeit/Stunde und fürs Datum
+        private DateTime _date = DateTime.UtcNow;
+
+        private WeatherCondition _condition;
+
+       
+        public string Temperatur
+        {
+            get { return _temperatur; }
+            set
+            {
+                _temperatur = value;
+                OnPropertyChanged(nameof(Temperatur));
+            }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set
+            {
+                _date = value;
+                OnPropertyChanged(nameof(Date));
+            }
+        }
+
+        public WeatherCondition Condition
+        {
+            get { return _condition; }
+            set
+            {
+                _condition = value;
+                OnPropertyChanged(nameof(Condition));
+            }
+        }
+    }
+}
